89082f29bca29ffc11ee1fa8e76e8b34d7c4be1d
tetsuo <ogunduz@gmail.com>
2015-04-26 23:24:44 +0300

    Release 0.1.3

--- setup.py
+++ setup.py
@@ -2,12 +2,12 @@ from setuptools import setup, find_packages
 
 setup(
     name='pv',
-    version='0.1.1',
+    version='0.1.3',
     description='minimal wsgi framework',
     url='http://github.com/tetsuo/pv',
     author='Onur Gunduz',
     author_email='ogunduz@gmail.com',
     packages=find_packages(),
-    install_requires=['simplejson', 'webob'],
+    install_requires=['simplejson', 'webob==1.4.1'],
     license='mit',
 )
